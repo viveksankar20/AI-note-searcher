746d4dd9ba9776ffaede63fe25c58406a68c6137
Vivek Sankar <154234096+viveksankar20@users.noreply.github.com>
2025-09-26 10:26:19 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
+    port: 5173,
+  },
+  build: {
+    outDir: 'dist',
+  },
+  preview: {
+    port: 4173,
   }
 })
